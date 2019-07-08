5e23d59c00eb315fc68b6f1a456e5bb9575c61c3
rciszek <ciszek@uef.fi>
2019-07-08 21:17:00 +0300

    Add genpath for dependencies

--- WaspFacer.m
+++ WaspFacer.m
@@ -9,6 +9,9 @@ function WaspFacer()
 %
 % Author: Robert Ciszek 
 % July 2015; Last revision: 30-May-2017
+
+    addpath(genpath(pwd))
+
     image_map = containers.Map('UniformValues',false);
     preprocessed_map = containers.Map('UniformValues',false);
     result_metadata_map = containers.Map('UniformValues',false);
